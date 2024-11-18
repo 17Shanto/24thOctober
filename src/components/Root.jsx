3d82438072bafe529e0920f4fcdbcd001f2095e6
17Shanto <shahporanshanto23@gmail.com>
2024-11-18 15:53:46 +0600

    Layout

--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from './Navbar';
 import { Outlet } from 'react-router-dom';
+import Layout from './Theater/Layout';
 
 const Root = () => {
     return (
         <div>
             <Navbar></Navbar>
             <Outlet></Outlet>
+            <Layout></Layout>
         </div>
     );
 };
---
+++ src/components/Theater/ChitChat.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ChitChat = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default ChitChat;
